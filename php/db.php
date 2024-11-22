5a5d5f8ba4ac66539dbc386f64707f096eccba51
Andreas Mäkinen <andreas.makinen@edu.huddinge.se>
2024-11-22 12:37:57 +0100

    commit

--- php/db.php
+++ php/db.php
@@ -4,19 +4,12 @@ ini_set('display_errors', 0);
 function Användarinformation() {
     // Detect the server environment
     $host = $_SERVER['HTTP_HOST'];
-
-    // Configure database connection settings based on the environment
-    if ($host === 'localhost' || $host === '127.0.0.1') {
-        // Localhost environment
-        $servername = "localhost";
-        $dbUsername = "samet";
-        $dbPassword = "samet";
-        $dbname = "användarinformation";
-    } else if ($host === '192.168.49.238' || strpos($host, '192.168.') === 0) {
+    
+    if ($host === '192.168.49.238' || strpos($host, '192.168.') === 0) {
         // Remote environment within the network
         $servername = "192.168.49.238"; // IP address of the database server
-        $dbUsername = "andreas";
-        $dbPassword = "andreas";
+        $dbUsername = "samet";
+        $dbPassword = "samet";
         $dbname = "användarinformation";
     } else {
         // Add more cases if needed for other environments
